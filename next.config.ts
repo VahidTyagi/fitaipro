b0685791ef1bcaa61c79626d6a0da9e16b21e2cb
unknown <vahidtyagi007@gmail.com>
2026-05-01 22:03:58 +0530

    feat: add local exercise

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "exercisedb-api.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.exercisedb.dev", // if using paid V2 CDN
+      },
+    ],
+  },
+};
 
 export default nextConfig;
